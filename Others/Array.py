b9c890d0b31d81707f97b6e0c2af68194d6d9c87
Aniruddha Saha <aniruddha.saha.7140@gmail.com>
2022-05-22 14:45:25 +0530

    Adding Binary Search Implementations

--- Others/Array.py
+++ Others/Array.py
@@ -7,23 +7,23 @@
 def array():
     try:
         # Use a breakpoint in the code line below to debug your script.
-        a=[1.00, 2, 3, 4] # Can store all types of variables in python
-        a[1]=6
+        a = [1.00, 2, 3, 4]  # Can store all types of variables in python
+        a[1] = 6
         print(a[:])  # Fetch all elements of an array
         print(a[:-1])  # Fetch all elements expect the last element of an array
         print(f'Hi, {"Arya"}')  # Press Ctrl+F8 to toggle the breakpoint.
 
         # Linear Search O(N)
 
-        max  = a[0]
+        max_id = a[0]
         for i in a:
-            if i > max :
-                max  = i
+            if i > max_id:
+                max_id = i
 
-        c='Bob Marley'
+        c = 'Bob Marley'
         print(c.split())
 
-        print(max )
+        print(max_id)
     except TypeError as e:
         print(f"Caught exception", {e}, {type(e)})
 
