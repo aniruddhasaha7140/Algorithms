6e3f22edce6dfee579888c8497a5d36dbf943bdb
Aniruddha Saha <aniruddha.saha.7140@gmail.com>
2023-05-05 11:24:39 +0530

    C++ Programming Basics

---
+++ C++ Programmimg STL Basics/input_name.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string first_name,title;
+    cout<< "Enter Name" <<endl;
+    cin>>first_name;
+
+    cout<< "Last Name" <<endl;
+    cin>>title;
+
+    cout<< "Full Name :" << first_name << " " << title << endl; 
+
+}
